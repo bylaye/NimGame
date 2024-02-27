706a7a7d0f7b9089db0a57b35709089492f94aea
bylaye <abdoulayeniang2010@gmail.com>
2024-02-27 20:21:17 +0000

    update play run

--- play.py
+++ play.py
@@ -4,14 +4,15 @@ from NimGame import NimGame
 game = NimGame('Player 1', 'Player 2', 10)
 
 """
-Passer 3 parametres pour representer
-le player 1, player 2, et le nombre d'objet
+Passer en parametre le nombre d'objet
 """
 if __name__ == '__main__':
-    if len(sys.argv) == 4:
-        number = int(sys.argv[3])
-        player1 = sys.argv[1]
-        player2 = sys.argv[2]
-        game = NimGame(player1, player2, number)
+    if len(sys.argv) == 2:
+        number = int(sys.argv[1])
+    else:
+        number = 20
+    player1 = 'Player 1'
+    player2 = 'Player 2'
+    game = NimGame(player1, player2, number)
     print(f'Total stack = {game.get_number()}')
     game.play()
